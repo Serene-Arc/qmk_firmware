bfa4d4ac79dc964e6ca792d9187927361bcc9aac
serene-arc <serenical@gmail.com>
2020-04-23 12:14:25 +1000

    Make changes to the keymap

--- keyboards/keebio/iris/keymaps/serene-arc/keymap.c
+++ keyboards/keebio/iris/keymaps/serene-arc/keymap.c
@@ -9,16 +9,21 @@
 bool is_alt_tab_active = false;
 uint16_t alt_tab_timer = 0;
 
+//Tap Dance Declarations
+enum {
+	TD_ENT_INS = 0
+};
+
 enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
-  LOWER,
-  RAISE,
-  ADJUST,
+	QWERTY = SAFE_RANGE,
+	LOWER,
+	RAISE,
+	ADJUST,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [_QWERTY] = LAYOUT(
+	[_QWERTY] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐							 ┌────────┬────────┬────────┬────────┬────────┬────────┐
 	 KC_ESC,  KC_1,    KC_2,	KC_3,	 KC_4,	  KC_5,								  KC_6,    KC_7,	KC_8,	 KC_9,	  KC_0,    KC_BSPC,
   //├────────┼────────┼────────┼────────┼────────┼────────┤							 ├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -26,13 +31,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤							 ├────────┼────────┼────────┼────────┼────────┼────────┤
 	 KC_LCTL, KC_A,    KC_S,	KC_D,	 KC_F,	  KC_G,								  KC_H,    KC_J,	KC_K,	 KC_L,	  KC_SCLN, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐		┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-	 KC_LSFT, KC_Z,    KC_X,	KC_C,	 KC_V,	  KC_B,    KC_GRV,			 KC_DEL,  KC_N,    KC_M,	KC_COMM, KC_DOT,  KC_SLSH, S(KC_INSERT),
+	 KC_LSFT, KC_Z,    KC_X,	KC_C,	 KC_V,	  KC_B,    KC_GRV,			 KC_DEL,  KC_N,    KC_M,	KC_COMM, KC_DOT,  KC_SLSH, TD(TD_ENT_INS),
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘		└───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-									KC_LGUI, LOWER,   KC_ENT,					 KC_SPC,  RAISE,   KC_LALT
+									KC_LGUI, LOWER,   KC_ENT,					 LSFT_T(KC_SPC),  RAISE,   KC_LALT
 								// └────────┴────────┴────────┘					└────────┴────────┴────────┘
-  ),
+	),
 
-  [_LOWER] = LAYOUT(
+	[_LOWER] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐							 ┌────────┬────────┬────────┬────────┬────────┬────────┐
 	 KC_TILD, KC_EXLM, KC_AT,	KC_HASH, KC_DLR,  KC_PERC,							  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PGUP,
   //├────────┼────────┼────────┼────────┼────────┼────────┤							 ├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -44,9 +49,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘		└───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
 									_______, _______, KC_DEL,					 KC_DEL,  _______, KC_P0
 								// └────────┴────────┴────────┘					└────────┴────────┴────────┘
-  ),
+	),
 
-  [_RAISE] = LAYOUT(
+	[_RAISE] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐							 ┌────────┬────────┬────────┬────────┬────────┬────────┐
 	 KC_F12,  KC_F1,   KC_F2,	KC_F3,	 KC_F4,   KC_F5,							  KC_F6,   KC_F7,	KC_F8,	 KC_F9,   KC_F10,  KC_F11,
   //├────────┼────────┼────────┼────────┼────────┼────────┤							 ├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -58,9 +63,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘		└───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
 									_______, _______, _______,					 _______, _______, _______
 								// └────────┴────────┴────────┘					└────────┴────────┴────────┘
-  ),
+	),
 
-  [_ADJUST] = LAYOUT(
+	[_ADJUST] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐							 ┌────────┬────────┬────────┬────────┬────────┬────────┐
 	 _______, _______, _______, _______, _______, _______,							  _______, _______, _______, _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤							 ├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -72,47 +77,47 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘		└───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
 									_______, _______, _______,					 _______, _______, _______
 								// └────────┴────────┴────────┘					└────────┴────────┴────────┘
-  )
+	)
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-	case QWERTY:
-	  if (record->event.pressed) {
-		set_single_persistent_default_layer(_QWERTY);
-	  }
-	  return false;
-	  break;
-	case LOWER:
-	  if (record->event.pressed) {
-		layer_on(_LOWER);
-		update_tri_layer(_LOWER, _RAISE, _ADJUST);
-	  } else {
-		layer_off(_LOWER);
-		update_tri_layer(_LOWER, _RAISE, _ADJUST);
-	  }
-	  return false;
-	  break;
-	case RAISE:
-	  if (record->event.pressed) {
-		layer_on(_RAISE);
-		update_tri_layer(_LOWER, _RAISE, _ADJUST);
-	  } else {
-		layer_off(_RAISE);
-		update_tri_layer(_LOWER, _RAISE, _ADJUST);
-	  }
-	  return false;
-	  break;
-	case ADJUST:
-	  if (record->event.pressed) {
-		layer_on(_ADJUST);
-	  } else {
-		layer_off(_ADJUST);
-	  }
-	  return false;
-	  break;
-  }
-  return true;
+	switch (keycode) {
+		case QWERTY:
+			if (record->event.pressed) {
+				set_single_persistent_default_layer(_QWERTY);
+			}
+			return false;
+			break;
+		case LOWER:
+			if (record->event.pressed) {
+				layer_on(_LOWER);
+				update_tri_layer(_LOWER, _RAISE, _ADJUST);
+			} else {
+				layer_off(_LOWER);
+				update_tri_layer(_LOWER, _RAISE, _ADJUST);
+			}
+			return false;
+			break;
+		case RAISE:
+			if (record->event.pressed) {
+				layer_on(_RAISE);
+				update_tri_layer(_LOWER, _RAISE, _ADJUST);
+			} else {
+				layer_off(_RAISE);
+				update_tri_layer(_LOWER, _RAISE, _ADJUST);
+			}
+			return false;
+			break;
+		case ADJUST:
+			if (record->event.pressed) {
+				layer_on(_ADJUST);
+			} else {
+				layer_off(_ADJUST);
+			}
+			return false;
+			break;
+	}
+	return true;
 }
 
 void encoder_update_user(uint8_t index, bool clockwise) {
@@ -142,10 +147,14 @@ void encoder_update_user(uint8_t index, bool clockwise) {
 	}
 }
 void matrix_scan_user(void) {
-  if (is_alt_tab_active) {
-	if (timer_elapsed(alt_tab_timer) > 1000) {
-	  unregister_code(KC_LALT);
-	  is_alt_tab_active = false;
+	if (is_alt_tab_active) {
+		if (timer_elapsed(alt_tab_timer) > 1000) {
+			unregister_code(KC_LALT);
+			is_alt_tab_active = false;
+		}
 	}
-  }
 }
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+	[TD_ENT_INS]  = ACTION_TAP_DANCE_DOUBLE(KC_ENTER, S(KC_INS))
+};

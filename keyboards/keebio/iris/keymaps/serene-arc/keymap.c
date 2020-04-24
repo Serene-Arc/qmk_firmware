20dc1d4ca8b1d8f99d284961288cc7507e4470c7
serene-arc <serenical@gmail.com>
2020-04-24 17:23:21 +1000

    Add further changes to keymap

--- keyboards/keebio/iris/keymaps/serene-arc/keymap.c
+++ keyboards/keebio/iris/keymaps/serene-arc/keymap.c
@@ -12,8 +12,24 @@ uint16_t alt_tab_timer = 0;
 //Tap Dance Declarations
 enum {
 	TD_ENT_INS = 0,
-	TD_8_AST
+	TD_8_AST,
+	TD_SPC_SENT
 };
+
+// define a type containing as many tapdance states as you need
+typedef enum {
+  SINGLE_TAP,
+  SINGLE_HOLD,
+  DOUBLE_TAP
+} td_state_t;
+
+// create a global instance of the tapdance state type
+static td_state_t td_state;
+
+// function to determine the current tapdance state
+int cur_dance (qk_tap_dance_state_t *state);
+
+void td_spacsent_fun (qk_tap_dance_state_t *state, void *user_data);
 
 enum custom_keycodes {
 	QWERTY = SAFE_RANGE,
@@ -32,17 +48,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤							 ├────────┼────────┼────────┼────────┼────────┼────────┤
 	 OSM(MOD_LCTL), KC_A,    KC_S,	KC_D,	 KC_F,	  KC_G,							  KC_H,    KC_J,	KC_K,	 KC_L,	  KC_SCLN, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐		┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-	 OSM(MOD_LSFT), KC_Z,    KC_X,	KC_C,	 KC_V,	  KC_B,	KC_GRV,			 KC_DEL,  KC_N,    KC_M,	KC_COMM, KC_DOT,  KC_SLSH, TD(TD_ENT_INS),
+	 OSM(MOD_LSFT), KC_Z,    KC_X,	KC_C,	 KC_V,	  KC_B,	KC_GRV,			 KC_MPLY,  KC_N,    KC_M,	KC_COMM, KC_DOT,  KC_SLSH, TD(TD_ENT_INS),
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘		└───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-									KC_LGUI, OSL(_LOWER), SFT_T(KC_ENT),	 	 KC_SPC,  OSL(_RAISE),   OSM(MOD_LALT)
+									KC_LGUI, OSL(_LOWER), SFT_T(KC_ENT),	 	 TD(TD_SPC_SENT),  OSL(_RAISE),   OSM(MOD_LALT)
 								// └────────┴────────┴────────┘					└────────┴────────┴────────┘
 	),
 
 	[_LOWER] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐							 ┌────────┬────────┬────────┬────────┬────────┬────────┐
-	 KC_TILD, KC_EXLM, KC_AT,	KC_HASH, KC_DLR,  KC_PERC,							  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PGUP,
+	 KC_TILD, KC_EXLM, KC_AT,	KC_HASH, KC_DLR,  KC_PERC,							  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
   //├────────┼────────┼────────┼────────┼────────┼────────┤							 ├────────┼────────┼────────┼────────┼────────┼────────┤
-	 RESET,   KC_1,    KC_2,	KC_3,	 KC_4,	  KC_5,								  KC_6,    KC_7,	KC_8,	 KC_9,	  KC_0,    KC_PGDN,
+	 RESET,   KC_1,    KC_2,	KC_3,	 KC_4,	  KC_5,								  KC_6,    KC_7,	KC_8,	 KC_9,	  KC_PGDN, KC_PGUP,
   //├────────┼────────┼────────┼────────┼────────┼────────┤							 ├────────┼────────┼────────┼────────┼────────┼────────┤
 	 KC_DEL,  _______, KC_LEFT, KC_RGHT, KC_UP,   KC_LBRC,							  KC_RBRC, KC_P4,	KC_P5,	 KC_P6,   KC_PLUS, KC_HOME,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐		┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
@@ -126,7 +142,31 @@ void matrix_scan_user(void) {
 	}
 }
 
+int cur_dance (qk_tap_dance_state_t *state) {
+	if (state->count == 1) {
+		if (state->interrupted || !state->pressed) { return SINGLE_TAP; }
+		else { return SINGLE_HOLD; }
+	}
+	if (state->count == 2) { return DOUBLE_TAP; }
+	else { return 3; } // any number higher than the maximum state value you return above
+}
+
+void td_spacsent_fun (qk_tap_dance_state_t *state, void *user_data){
+	td_state = cur_dance(state);
+	switch (td_state) {
+		case SINGLE_TAP:
+			tap_code16(KC_SPACE);
+			break;
+		case DOUBLE_TAP:
+			SEND_STRING(". ");
+			break;
+		default:
+			break;
+	}
+}
+
 qk_tap_dance_action_t tap_dance_actions[] = {
 	[TD_ENT_INS]  = ACTION_TAP_DANCE_DOUBLE(KC_ENTER, S(KC_INS)),
-	[TD_8_AST]  = ACTION_TAP_DANCE_DOUBLE(KC_8, KC_PAST)
+	[TD_8_AST]  = ACTION_TAP_DANCE_DOUBLE(KC_8, KC_PAST),
+	[TD_SPC_SENT] = ACTION_TAP_DANCE_FN(td_spacsent_fun) 
 };

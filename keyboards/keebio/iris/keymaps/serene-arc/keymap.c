23adb6e00d698781638c164242b7efa25923d14b
serene-arc <serenical@gmail.com>
2020-04-24 11:26:05 +1000

    Add asterisk tap dance

--- keyboards/keebio/iris/keymaps/serene-arc/keymap.c
+++ keyboards/keebio/iris/keymaps/serene-arc/keymap.c
@@ -11,7 +11,8 @@ uint16_t alt_tab_timer = 0;
 
 //Tap Dance Declarations
 enum {
-	TD_ENT_INS = 0
+	TD_ENT_INS = 0,
+	TD_8_AST
 };
 
 enum custom_keycodes {
@@ -25,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[_QWERTY] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐							 ┌────────┬────────┬────────┬────────┬────────┬────────┐
-	 KC_ESC,  KC_1,    KC_2,	KC_3,	 KC_4,	  KC_5,								  KC_6,    KC_7,	KC_8,	 KC_9,	  KC_0,    KC_BSPC,
+	 KC_ESC,  KC_1,    KC_2,	KC_3,	 KC_4,	  KC_5,								  KC_6,    KC_7,	TD(TD_8_AST),	 KC_9,	  KC_0,    KC_BSPC,
   //├────────┼────────┼────────┼────────┼────────┼────────┤							 ├────────┼────────┼────────┼────────┼────────┼────────┤
 	 KC_TAB,  KC_Q,    KC_W,	KC_E,	 KC_R,	  KC_T,								  KC_Y,    KC_U,	KC_I,	 KC_O,	  KC_P,    KC_BSLS,
   //├────────┼────────┼────────┼────────┼────────┼────────┤							 ├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -33,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐		┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
 	 OSM(MOD_LSFT), KC_Z,    KC_X,	KC_C,	 KC_V,	  KC_B,	KC_GRV,			 KC_DEL,  KC_N,    KC_M,	KC_COMM, KC_DOT,  KC_SLSH, TD(TD_ENT_INS),
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘		└───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-									KC_LGUI, LOWER,   SFT_T(KC_ENT),			 KC_SPC,  RAISE,   OSM(MOD_LALT)
+									KC_LGUI, OSL(_LOWER), SFT_T(KC_ENT),	 	 KC_SPC,  OSL(_RAISE),   OSM(MOD_LALT)
 								// └────────┴────────┴────────┘					└────────┴────────┴────────┘
 	),
 
@@ -47,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐		┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
 	 BL_STEP, _______, _______, _______, KC_DOWN, KC_LCBR, KC_LPRN,			 KC_RPRN, KC_RCBR, KC_P1,	KC_P2,	 KC_P3,   KC_MINS, KC_END,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘		└───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-									_______, _______, KC_DEL,					 KC_DEL,  _______, KC_LOCK
+									_______, _______, KC_DEL,					 KC_DEL,  _______, KC_P0
 								// └────────┴────────┴────────┘					└────────┴────────┴────────┘
 	),
 
@@ -67,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[_ADJUST] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐							 ┌────────┬────────┬────────┬────────┬────────┬────────┐
-	 _______, _______, _______, _______, _______, _______,							  _______, _______, _______, _______, _______, _______,
+	 KC_LOCK, _______, _______, _______, _______, _______,							  _______, _______, _______, _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤							 ├────────┼────────┼────────┼────────┼────────┼────────┤
 	 _______, _______, _______, _______, _______, _______,							  _______, _______, _______, _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤							 ├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -82,42 +83,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 	switch (keycode) {
-		case QWERTY:
-			if (record->event.pressed) {
-				set_single_persistent_default_layer(_QWERTY);
-			}
-			return false;
-			break;
-		case LOWER:
-			if (record->event.pressed) {
-				layer_on(_LOWER);
-				update_tri_layer(_LOWER, _RAISE, _ADJUST);
-			} else {
-				layer_off(_LOWER);
-				update_tri_layer(_LOWER, _RAISE, _ADJUST);
-			}
-			return false;
-			break;
-		case RAISE:
-			if (record->event.pressed) {
-				layer_on(_RAISE);
-				update_tri_layer(_LOWER, _RAISE, _ADJUST);
-			} else {
-				layer_off(_RAISE);
-				update_tri_layer(_LOWER, _RAISE, _ADJUST);
-			}
-			return false;
-			break;
-		case ADJUST:
-			if (record->event.pressed) {
-				layer_on(_ADJUST);
-			} else {
-				layer_off(_ADJUST);
-			}
-			return false;
-			break;
+
 	}
 	return true;
+}
+layer_state_t layer_state_set_user(layer_state_t state) {
+	return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
 
 void encoder_update_user(uint8_t index, bool clockwise) {
@@ -156,5 +127,6 @@ void matrix_scan_user(void) {
 }
 
 qk_tap_dance_action_t tap_dance_actions[] = {
-	[TD_ENT_INS]  = ACTION_TAP_DANCE_DOUBLE(KC_ENTER, S(KC_INS))
+	[TD_ENT_INS]  = ACTION_TAP_DANCE_DOUBLE(KC_ENTER, S(KC_INS)),
+	[TD_8_AST]  = ACTION_TAP_DANCE_DOUBLE(KC_8, KC_PAST)
 };

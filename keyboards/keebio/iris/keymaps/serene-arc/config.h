46b7c8972f1d0e65aa5f07547ef229b39560e148
serene-arc <serenical@gmail.com>
2020-05-02 14:29:35 +1000

    Disable unneeded features to save space

--- keyboards/keebio/iris/keymaps/serene-arc/config.h
+++ keyboards/keebio/iris/keymaps/serene-arc/config.h
@@ -77,3 +77,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #undef RGBLIGHT_ANIMATIONS
 #define ONESHOT_TAP_TOGGLE 3 // Tapping this number of times holds the key until tapped once again
+#define NO_DEBUG
+#define NO_PRINT

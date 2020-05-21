e5d51f8ebf576393b6a643e091fe29ac56b3c7fb
serene-arc <serenical@gmail.com>
2020-05-21 13:20:41 +1000

    Update the script to terminate if a prev step fails

--- update-master.sh
+++ update-master.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git checkout master
-git fetch upstream
-git pull upstream master
-git push origin master
+git checkout master && \
+git fetch upstream && \
+git pull upstream master && \
+git push origin master && \
 git checkout development

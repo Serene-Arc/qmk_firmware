77f78c9f62b2925e090fbceb932fc26ff2e938e8
Serene-Arc <serenical@gmail.com>
2023-04-30 13:27:12 +1000

    Update keyboard map and settings

--- keyboards/keebio/kbo5000/keymaps/serene-arc/keymap.c
+++ keyboards/keebio/kbo5000/keymaps/serene-arc/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	CLEAR_MOD,  KC_F1,   KC_F2,	KC_F3,	 KC_F4,   KC_F5,   KC_F6,			 		 KC_F7,   KC_F8,			KC_F9,	 KC_F10,  KC_F11,  KC_F12,	KC_PSCR, KC_MNXT, KC_MPLY,
 	WIN_SWCH,KC_ESC,  KC_1,    KC_2,	KC_3,	 KC_4,	  KC_5,    KC_6,			 KC_7,	  KC_8,    KC_9,	KC_0,	 KC_MINS, KC_EQL,  KC_BSPC,	KC_DEL, KC_INS,  KC_PGUP,
 	A_ESC,	 KC_TAB,  KC_Q,    KC_W,	KC_E,	 KC_R,	  KC_T,						 KC_Y,	  KC_U,    KC_I,	KC_O,	 KC_P,	  KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,  KC_PGDN,
-	C(A(KC_T)),   KC_CAPS, KC_A,    KC_S,	KC_D,	 KC_F,	  KC_G,					 KC_H,	  KC_J,    KC_K,	KC_L,	 KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,  KC_HOME, KC_END,
+	C(A(KC_T)),   KC_NO, KC_A,    KC_S,	KC_D,	 KC_F,	  KC_G,					 KC_H,	  KC_J,    KC_K,	KC_L,	 KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,  KC_HOME, KC_END,
 	TAB_SWTH,  KC_LSPO, KC_GRV,  KC_Z,	KC_X,	 KC_C,	  KC_V,    KC_B,			 KC_N,	  KC_M,    KC_COMM, KC_DOT,  KC_SLSH,		   KC_RSPC,			 KC_UP,
 	KC_MS_BTN3,  KC_LCTL, TD(TD_BRACK_O), KC_LGUI, MO(1),	 SFT_T(KC_ENT),  KC_SPC,	 MO(1),   TD(TD_SPC_SENT),  TD(TD_BRACK_C), KC_LGUI, KC_LCTL, KC_LEFT, KC_DOWN, KC_RGHT
   ),
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	RESET,			  RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD,			 _______, _______,			_______, _______, _______, _______, _______, _______, _______,
 	RGB_TOG, _______, VIM_N1, VIM_N2, VIM_N3, VIM_N4, VIM_N5, VIM_N6,			 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
 	RGB_MOD, _______, _______, _______, _______, _______, _______,					 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-	_______, _______, _______, _______, _______, _______, _______,					 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+	_______, KC_CAPS, _______, _______, _______, _______, _______,					 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
 	_______, _______, _______, _______, _______, _______, _______, _______,			 _______, _______, _______, _______, _______,		   _______,			 TD(DISC_UP),
 	_______, _______, _______, _______, _______, _______, _______,					 _______, _______, _______, _______,				   _______, KC_VOLD, TD(DISC_DN), KC_VOLU
   ),

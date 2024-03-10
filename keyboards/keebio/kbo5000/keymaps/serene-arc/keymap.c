903d4aa769bf4c0d5915c611bcded9854eb8de98
Serene-Arc <33189705+Serene-Arc@users.noreply.github.com>
2024-03-10 12:18:47 +1000

    Add shortcut

--- keyboards/keebio/kbo5000/keymaps/serene-arc/keymap.c
+++ keyboards/keebio/kbo5000/keymaps/serene-arc/keymap.c
@@ -39,7 +39,8 @@ enum custom_keycodes {
 	ENQUOTE,
 	EMPH,
 	LATEX_CHAP,
-    MATH_I
+    MATH_I,
+    LATEX_T
 };
 
 // create a global instance of the tapdance state type
@@ -67,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [1] = LAYOUT_all(
 	QK_BOOT,			  RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD,			 _______, _______,			_______, _______, _______, _______, _______, _______, _______,
 	RGB_TOG, _______, VIM_N1, VIM_N2, VIM_N3, VIM_N4, VIM_N5, VIM_N6,			 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-	RGB_MOD, _______, _______, _______, TD(LATEX_E), _______, _______,					 _______, _______, MATH_I, _______, _______, _______, _______, _______, _______, _______,
+	RGB_MOD, _______, _______, _______, TD(LATEX_E), _______, LATEX_T,					 _______, _______, MATH_I, _______, _______, _______, _______, _______, _______, _______,
 	_______, KC_CAPS, _______, TD(LATEX_S), _______, _______, _______,					 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
 	_______, _______, _______, _______, _______, LATEX_CHAP, _______, _______,			 _______, _______, _______, _______, _______,		   _______,			 TD(DISC_UP),
 	_______, _______, _______, _______, _______, _______, _______,					 _______, _______, _______, _______,				   _______, KC_MPRV, TD(DISC_DN), KC_MNXT
@@ -164,6 +165,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 		case VIM_N7:
 			if (record->event.pressed) {
 				SEND_STRING("$!nm7");
+			}
+			break;
+		case LATEX_T:
+			if (record->event.pressed) {
+				SEND_STRING("\\texttt{");
 			}
 			break;
 		case MATH_I:

9904e35d279b55e6f2a7b440409aa9e637e6d899
Serene-Arc <33189705+Serene-Arc@users.noreply.github.com>
2024-09-26 11:02:15 +1000

    Add keybind

--- keyboards/keebio/kbo5000/keymaps/serene-arc/keymap.c
+++ keyboards/keebio/kbo5000/keymaps/serene-arc/keymap.c
@@ -50,6 +50,7 @@ enum custom_keycodes {
     LATEX_X,
     MATH_1,
     MATH_2,
+    MATH_3,
     ALIGN_EQ,
     ESC_UND,
     MINT_IN,
@@ -79,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [1] = LAYOUT_all(
-    QK_BOOT,              MATH_1, MATH_2, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD,            _______, _______,          _______, _______, _______, _______, _______, _______, _______,
+    QK_BOOT,              MATH_1, MATH_2, MATH_3, RGB_SAD, RGB_VAI, RGB_VAD,            _______, _______,          _______, _______, _______, _______, _______, _______, _______,
     RGB_TOG, _______, VIM_N1, VIM_N2, VIM_N3, VIM_N4, VIM_N5, VIM_N6,            _______, _______, _______, _______, ESC_UND, ALIGN_EQ, _______, _______, _______, _______,
     RGB_MOD, _______, _______, _______, TD(LATEX_E), LATEX_R, LATEX_T,                   _______, LATEX_U, MATH_I, _______, LATEX_P, _______, _______, _______, _______, _______,
     _______, KC_CAPS, _______, TD(LATEX_S), _______, LATEX_F, _______,                   _______, MATH_J, LATEX_K, _______, _______, _______, _______, _______, _______, _______,
@@ -228,6 +229,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case MATH_2:
             if (record->event.pressed) {
                 SEND_STRING("^2");
+            }
+            break;
+        case MATH_3:
+            if (record->event.pressed) {
+                SEND_STRING("^3");
             }
             break;
         case ESC_UND:
